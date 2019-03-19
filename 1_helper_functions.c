89db949e647ca6aefee057297ffce9f0c0e83569
komerela <rmwiti@gmail.com>
2019-03-19 21:58:30 +0000

    Helper function for decing and int

--- 1_helper_functions.c
+++ 1_helper_functions.c
@@ -2,23 +2,11 @@
 
 /**
  * print_integer - print integer
- * @args: arguments
- * Return: void
+ * @all_parameters: arguments
+ * Return: int otherwise -1
  */
 
-int print_decint(va_list all_parameters)
-{
-	_putchar(va_arg(all_parameters, int));
-	return (1);
-}
-
-/**
- * print_integer - print integer
- * @args: arguments
- * Return: void
- */
-
-int print_int(va_arg_arg(all_parameters, int));
+int print_integer(va_list all_parameters)
 {
 	_putchar(va_arg(all_parameters, int));
 	return (1);
